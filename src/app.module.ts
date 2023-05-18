c48d2d47793613b42d922175ac2b4485fb5e9f46
Luis Durante <luisdurante2@outlook.com>
2023-05-18 12:17:48 -0300

    :card_file_box: Add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_HOST, {
+      useNewUrlParser: true,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD,
+      dbName: process.env.DB_NAME,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

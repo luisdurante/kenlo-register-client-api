1ee9ad266a38ab0f7dcc71ff1cedcd20ae12753c
Luis Durante <luisdurante2@outlook.com>
2023-05-18 20:18:36 -0300

    :technologist: Create database module to use it better on tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { ClientsModule } from './clients/clients.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_HOST, {
-      useNewUrlParser: true,
-      user: process.env.DB_USER,
-      pass: process.env.DB_PASSWORD,
-      dbName: process.env.DB_NAME,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     ClientsModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('NODE_ENV') === 'test'
+            ? configService.get<string>('DB_TEST_HOST')
+            : configService.get<string>('DB_HOST'),
+        useNewUrlParser: true,
+        user: configService.get<string>('DB_USER'),
+        pass: configService.get<string>('DB_PASSWORD'),
+        dbName:
+          configService.get<string>('NODE_ENV') === 'test'
+            ? configService.get<string>('DB_TEST_NAME')
+            : configService.get<string>('DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule {}

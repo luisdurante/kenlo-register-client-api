7b6b08c1dfb67168e7708cb2991674cb2e179680
Luis Durante <luisdurante2@outlook.com>
2023-05-18 17:39:55 -0300

    :safety_vest: Improve phoneNumber error message

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,5 +1,4 @@
 import { IsEmail, Matches, IsNotEmpty, IsString } from 'class-validator';
-
 export class CreateClientDto {
   @IsNotEmpty()
   @IsString()
@@ -10,7 +9,7 @@ export class CreateClientDto {
 
   @Matches(
     /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/,
-    { message: 'must be a valid brazilian number.' },
+    { message: 'phoneNumber must be a valid brazilian number.' },
   )
   readonly phoneNumber: string;
 }

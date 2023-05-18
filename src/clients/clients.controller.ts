cbfe7ac2b9069cfdf1abe7c1607853dbc244ce8d
Luis Durante <luisdurante2@outlook.com>
2023-05-18 20:19:30 -0300

    :adhesive_bandage: Fix HTTP methods responses

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 
@@ -7,11 +14,13 @@ export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientsService.insertOne(createClientDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   async findAll() {
     const clients = await this.clientsService.findAll();
     return { clients };

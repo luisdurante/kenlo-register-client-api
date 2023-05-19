7ffc7f7cac0a71ad01052bb7ccefa7943648c1b7
Luis Durante <luisdurante2@outlook.com>
2023-05-19 20:02:36 -0300

    :fire: Remove unused imports

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,8 +1,4 @@
-import {
-  ConflictException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
